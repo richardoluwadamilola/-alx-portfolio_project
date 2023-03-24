2d14701fcad6847335829519b314dac9c127fd41
Richard Oluwadamilola <oluwatobyrichard@gmail.com>
2023-03-24 10:27:23 +0100

    fix

--- src/wall.c
+++ src/wall.c
@@ -39,8 +39,8 @@ void renderFloor(int wallBottomPixel, color_t *texelColor, int x)
 		distance = (ratio * PROJ_PLANE)
 					/ cos(rays[x].rayAngle - player.rotationAngle);
 
-		textureOffsetY = (double)abs((distance * sin(rays[x].rayAngle)) + player.y);
-		textureOffsetX = (double)abs((distance * cos(rays[x].rayAngle)) + player.x);
+		textureOffsetY = (int)abs((distance * sin(rays[x].rayAngle)) + player.y);
+		textureOffsetX = (int)abs((distance * cos(rays[x].rayAngle)) + player.x);
 
 		textureOffsetX = (int)(abs(textureOffsetX * texture_width / 30)
 								% texture_width);
@@ -75,8 +75,8 @@ void renderCeil(int wallTopPixel, color_t *texelColor, int x)
 		distance = (ratio * PROJ_PLANE)
 					/ cos(rays[x].rayAngle - player.rotationAngle);
 
-		textureOffsetY = (double)abs((-distance * sin(rays[x].rayAngle)) + player.y);
-		textureOffsetX = (double)abs((-distance * cos(rays[x].rayAngle)) + player.x);
+		textureOffsetY = (int)abs((-distance * sin(rays[x].rayAngle)) + player.y);
+		textureOffsetX = (int)abs((-distance * cos(rays[x].rayAngle)) + player.x);
 
 		textureOffsetX = (int)(abs(textureOffsetX * texture_width / 40)
 								% texture_width);
